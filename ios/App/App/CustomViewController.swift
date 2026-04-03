1737614fdcf749774c01e9c719c1ec3364361eb9
Eythan D'Amico <eythandamico@mac.home>
2026-04-03 13:37:28 -0400

    Fix black screen: use viewDidLayoutSubviews + frame instead of constraints

--- ios/App/App/CustomViewController.swift
+++ ios/App/App/CustomViewController.swift
@@ -2,21 +2,11 @@ import UIKit
 import Capacitor
 
 class CustomViewController: CAPBridgeViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
 
         // Extend WebView behind safe areas (removes white bars)
         webView?.scrollView.contentInsetAdjustmentBehavior = .never
-
-        // Ensure WebView fills the entire screen
-        if let webView = webView {
-            webView.translatesAutoresizingMaskIntoConstraints = false
-            NSLayoutConstraint.activate([
-                webView.topAnchor.constraint(equalTo: view.topAnchor),
-                webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-                webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            ])
-        }
+        webView?.frame = view.bounds
     }
 }
